0ee8f739aae02736ba5e9b10ee88293d2ced7ed9
Felipe Soares <felipeluizsoares@gmail.com>
2017-02-02 16:23:37 -0200

    Add App component

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,45 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var App = React.createClass({
+   render: function () {
+     return (
+       <div className="catch-of-the-day">
+         <div className="menu">
+           <Header/>
+         </div>
+         <Order/>
+         <Inventory/>
+       </div>
+     )
+   }
+});
+
+var Header = React.createClass({
+  render: function () {
+    return (
+      <p> Header </p>
+    )
+  }
+});
+
+var Order = React.createClass({
+  render: function () {
+    return (
+      <p> Order </p>
+    )
+  }
+});
+
+
+var Inventory = React.createClass({
+  render: function () {
+    return (
+      <p> Inventory </p>
+    )
+  }
+});
+
 var StorePicker  = React.createClass({
   render: function () {
     return (
@@ -13,4 +52,4 @@ var StorePicker  = React.createClass({
   }
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
