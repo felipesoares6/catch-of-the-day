8d2f1507092638b8b8e2bbdd8f9e3a31f0ce546e
Felipe Soares <felipeluizsoares@gmail.com>
2017-02-05 14:51:04 -0200

    Add fishes quantity to order

--- scripts/main.js
+++ scripts/main.js
@@ -31,8 +31,13 @@ var App = React.createClass({
     })
   },
 
+  addToOrder: function (key) {
+    this.state.order[key] = this.state.order[key] + 1 || 1;
+    this.setState({order: this.state.order});
+  },
+
   renderFish: function (key) {
-    return <Fish key={key} index={key} details={this.state.fishes[key]} />
+    return <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>
   },
 
  render: function () {
@@ -52,8 +57,15 @@ var App = React.createClass({
 });
 
 var Fish = React.createClass({
+  onButtonClick: function () {
+    var key = this.props.index;
+    this.props.addToOrder(key);
+  },
+
   render: function () {
     var details = this.props.details;
+    var isAvailable = details.status === 'available' ? true : false;
+    var buttonText = isAvailable ? 'Add to Order' : 'Sold Out!';
 
     return (
       <li className="menu-fish">
@@ -63,6 +75,9 @@ var Fish = React.createClass({
           <span className="price"> {h.formatPrice(details.price)} </span>
         </h3>
         <p> {details.desc} </p>
+        <button disabled={!isAvailable} onClick={this.onButtonClick}>
+          {buttonText}
+        </button>
       </li>
     )
   }
